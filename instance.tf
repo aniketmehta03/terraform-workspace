eaecb3979b00f37593b49dbad7e711f3a468cb1c
aniketmehta03 <aniketmehta036@gmail.com>
2025-05-01 11:20:38 +0000

    modules

--- instance.tf
+++ instance.tf
@@ -1,4 +1,7 @@
-resource "aws_instance" "web" {
-  ami           = var.image_id
-  instance_type = var.instance_type
+module "mywebserve" {
+  source = "./modules/webbserves"
+  key = file("${path.module}/id_rsa.pub")
+  image_id = "${var.image_id}"
+  instance_type = "${t2.small}"
+  key_name = "mykey"
 }
---
+++ modules/webbserves/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "image_id" {
+type = string  
+}
+
+variable "key" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
